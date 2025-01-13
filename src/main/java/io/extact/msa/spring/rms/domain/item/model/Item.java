2c105e97e59ad4497c876a4dad559119097256c9
extact-io <ozytso@gmail.com>
2025-01-13 09:57:08 +0900

    update

--- src/main/java/io/extact/msa/spring/rms/domain/item/model/Item.java
+++ src/main/java/io/extact/msa/spring/rms/domain/item/model/Item.java
@@ -1,17 +1,10 @@
 package io.extact.msa.spring.rms.domain.item.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Valid;
 import jakarta.validation.Validator;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.groups.Default;
 
-import io.extact.msa.spring.platform.fw.domain.constraint.ValidationGroups.Add;
 import io.extact.msa.spring.platform.fw.domain.model.DomainModel;
-import io.extact.msa.spring.platform.fw.exception.RmsConstraintViolationException;
 import io.extact.msa.spring.rms.domain.item.constraint.ItemName;
 import io.extact.msa.spring.rms.domain.item.constraint.SerialNo;
 import lombok.EqualsAndHashCode;
@@ -40,19 +33,6 @@ public class Item implements DomainModel, ItemReference {
         this.itemName = itemName;
     }
 
-    @Override
-    public void configureValidator(Validator validator) {
-        this.validator = validator;
-    }
-
-    @Override
-    public void verify() {
-        // 生々しいのでvalidatorをなにかで被せる
-        Set<ConstraintViolation<Item>> result = this.validator.validate(this, Default.class, Add.class);
-        if (!result.isEmpty()) {
-            throw new RmsConstraintViolationException("validation error.", new HashSet<>(result));
-        }
-    }
 
     public interface ItemCreatable {
         default Item newInstance(
