bc02b232e38aa584dc272ad6730478a2bc8c3e42
extact-io <ozytso@gmail.com>
2025-09-15 14:14:03 +0900

    update

--- src/main/java/io/extact/msa/spring/rms/WebApiApplication.java
+++ src/main/java/io/extact/msa/spring/rms/WebApiApplication.java
@@ -1,10 +1,7 @@
 package io.extact.msa.spring.rms;
 
-import jakarta.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import io.extact.msa.spring.platform.core.CoreConfig;
@@ -13,8 +10,6 @@ import io.extact.msa.spring.rms.application.ApplicationServiceConfig;
 import io.extact.msa.spring.rms.domain.DomainConfig;
 import io.extact.msa.spring.rms.infrastructure.persistence.PersistenceConfig;
 import io.extact.msa.spring.rms.webapi.WebApiConfig;
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 
 @SpringBootConfiguration
 @Import({
@@ -28,26 +23,5 @@ public class WebApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApiApplication.class, args);
-    }
-
-    // TODO: Condationalにしてfwに持っていく
-    @Bean
-    OpenTelemetryAppenderInitializer openTelemetryAppenderInitializer(OpenTelemetry openTelemetry) {
-        return new OpenTelemetryAppenderInitializer(openTelemetry);
-    }
-
-    static class OpenTelemetryAppenderInitializer {
-
-        private final OpenTelemetry openTelemetry;
-
-        OpenTelemetryAppenderInitializer(OpenTelemetry openTelemetry) {
-            this.openTelemetry = openTelemetry;
-        }
-
-        @PostConstruct
-        void init() {
-            OpenTelemetryAppender.install(this.openTelemetry);
-        }
-
     }
 }
